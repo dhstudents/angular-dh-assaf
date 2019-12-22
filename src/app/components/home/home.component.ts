a99c714325add79d891696e91ac6fe23a7a77a37
dhstudents <dhgithub@gmail.com>
2019-12-22 16:29:47 +0200

    3 files modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  MMMyyyy = new Date();
+  discount = 10;
+  imageWidth : number = 300
+
+  constructor() { 
+    this.imageWidth = 300;
+  }
 
   ngOnInit() {
+  }
+
+  incImageWidth() {
+    this.imageWidth += 10;
+  }
+  decImageWidth() {
+    this.imageWidth -= 10;
+  }
+  resetImageWidth() {
+    this.imageWidth = 300;
   }
 
 }
