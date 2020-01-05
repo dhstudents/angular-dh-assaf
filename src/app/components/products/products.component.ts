e422d097da41d3fca61882a2dc2ff0ab534b0d5d
dhstudents <dhgithub@gmail.com>
2020-01-05 20:06:03 +0200

    1 file modified

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service'
+import { ProductService } from './src/app/services/product.service'
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -11,7 +11,7 @@ export class ProductsComponent implements OnInit {
   constructor(private productService : ProductService) { }
 
   ngOnInit() {
-    this.products = productService
+   // this.products = productService.
   }
 
   
