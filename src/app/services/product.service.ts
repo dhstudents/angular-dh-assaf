66a21ac6df36ed3ed36474a04490e25edcda3534
dhstudents <dhgithub@gmail.com>
2020-01-06 16:27:23 +0200

    1 file modified

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Product } from "../models/product";
+import { Observable } from 'rxjs'
 @Injectable({
   providedIn: "root"
 })
@@ -51,4 +52,36 @@ export class ProductService {
       }, 5000);
     });
   }
+
+  // observable (import { Observable } from 'rxjs' )
+  public getAllProductsAsyncObservable(): Observable<Product[]> {
+
+    return Observable.create( observer => {
+      setTimeout(() => {
+        try {
+          const productlist: Product[] = [];
+          productlist.push(new Product(1, "Apple", 3.5, 100));
+          productlist.push(new Product(2, "Banana", 2, 120));
+          productlist.push(new Product(3, "Peach", 10, 50));
+          productlist.push(new Product(4, "Watermelon", 6, 40));
+          //    return productlist;
+          observer.next(productlist);
+        } catch (error) {
+          observer.error(error)
+        }
+      }, 5000);
+
+    })
+
+
+
+
+  }
+
+
 }
+
+// https://angular.io/guide/observables
+// https://angular.io/guide/observables-in-angular
+// https://medium.com/@luukgruijs/understanding-creating-and-subscribing-to-observables-in-angular-426dbf0b04a3
+// https://appdividend.com/2018/12/08/angular-7-observables-tutorial-with-example/
