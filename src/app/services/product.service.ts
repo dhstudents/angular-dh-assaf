6330cf6f6dbb69a912fae4732fd9b7f364cd3d1b
dhstudents <dhgithub@gmail.com>
2020-01-06 14:18:20 +0200

    1 file modified

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -15,6 +15,7 @@ export class ProductService {
     return productlist;
   }
 
+  // callback
   public getAllProductsAsync(successCallback, failCallback): void {
     setTimeout(() => {
       try {
@@ -29,5 +30,25 @@ export class ProductService {
         failCallback(error);
       }
     }, 5000);
+  }
+  // promise
+  public getAllProductsAsyncPromise(): Promise<Product[]> {
+    // resolve = successCallback,
+    // reject  = failCallback
+    return new Promise((resolve /* successCallback*/, reject /* failCallback */) => {
+      setTimeout(() => {
+        try {
+          const productlist: Product[] = [];
+          productlist.push(new Product(1, "Apple", 3.5, 100));
+          productlist.push(new Product(2, "Banana", 2, 120));
+          productlist.push(new Product(3, "Peach", 10, 50));
+          productlist.push(new Product(4, "Watermelon", 6, 40));
+          //    return productlist;
+          resolve(productlist);
+        } catch (error) {
+          reject(error)
+        }
+      }, 5000);
+    });
   }
 }
