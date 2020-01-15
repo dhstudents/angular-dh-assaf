8fa13260406693ba8379032a5ebb03b840617cb3
dhstudents <dhgithub@gmail.com>
2020-01-15 19:06:53 +0200

    1 file modified

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -10,12 +11,16 @@ export class ProductDetailsComponent implements OnInit {
 
   product : Product;
 
-  constructor(private activatedRoute : ActivatedRoute) { }
+  constructor(
+    private activatedRoute : ActivatedRoute , 
+    private productService : ProductService 
+  ) { }
 
   ngOnInit() {
-  /  alert("Product details!!!");
+  //  alert("Product details!!!");
     let proid = this.activatedRoute.snapshot.params.id
-    alert(proid);
+  //  alert(proid);
+  
   }
 
 }
